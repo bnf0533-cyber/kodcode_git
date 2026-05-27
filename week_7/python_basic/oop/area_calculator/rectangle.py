a7774d0fca5591be94b8ab68de76fd48819ae547
bnf0533-cyber <bnf0533@gmail.com>
2026-05-27 17:51:33 +0300

    Add Rectangle class

---
+++ week_7/python_basic/oop/area_calculator/rectangle.py
@@ -0,0 +1,14 @@
+from calculator import Shape
+
+class Rectangle(Shape):
+    def __init__(self,width,height):
+        super().__init__(width,height)
+        self.width = width
+        self.height = height
+
+    def get_area(self):
+        return self.width * self.height
+    
+    def get_perimeter(self):
+        return (self.width + self.height) * 2
+    
