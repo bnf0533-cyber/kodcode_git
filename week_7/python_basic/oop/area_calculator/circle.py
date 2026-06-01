fa48eb2e3bfaae8e9e8830c53fa6cf9a95b87ee9
bnf0533-cyber <bnf0533@gmail.com>
2026-06-01 17:48:22 +0300

    add circle file

--- week_7/python_basic/oop/area_calculator/circle.py
+++ week_7/python_basic/oop/area_calculator/circle.py
@@ -1,8 +1,8 @@
 from shape import Shape
 from math import pi
 class Circle(Shape):
-    def __init__(self,radius):
-        super().__init__(radius)
+    def __init__(self,id,radius,*number):
+        super().__init__(id,"circle",radius)
         self.radius = radius
     
     def get_area(self):
