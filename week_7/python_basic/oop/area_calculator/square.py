f0858f7bc98c3d7779b411c92d7f30b591431b68
bnf0533-cyber <bnf0533@gmail.com>
2026-06-01 17:59:36 +0300

    add square file

--- week_7/python_basic/oop/area_calculator/square.py
+++ week_7/python_basic/oop/area_calculator/square.py
@@ -1,6 +1,7 @@
 from rectangle import  Rectangle
 class Square(Rectangle):
-    def __init__(self,side):
-        super().__init__(side,side)
+    def __init__(self,shape_id,side):
+        super().__init__(shape_id,side,side)
+        self.shape_type = "square"
         self.side = side
     
