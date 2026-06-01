7ef0a5365efd774eb8a4004c3c62d371a13db712
bnf0533-cyber <bnf0533@gmail.com>
2026-06-01 17:50:01 +0300

    add rectangle file

--- week_7/python_basic/oop/area_calculator/rectangle.py
+++ week_7/python_basic/oop/area_calculator/rectangle.py
@@ -1,8 +1,8 @@
 from shape import Shape
 
 class Rectangle(Shape):
-    def __init__(self,width,height):
-        super().__init__(width,height)
+    def __init__(self,shape_id,width,height):
+        super().__init__(shape_id,"rectangle",width,height)
         self.width = width
         self.height = height
 
