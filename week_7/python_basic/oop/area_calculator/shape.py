893f91567b882a3ab6bffe2410983e63eb84816f
bnf0533-cyber <bnf0533@gmail.com>
2026-06-01 17:38:12 +0300

    add shap file

--- week_7/python_basic/oop/area_calculator/shape.py
+++ week_7/python_basic/oop/area_calculator/shape.py
@@ -1,9 +1,11 @@
 
 class Shape:
     
-    def __init__(self,*number):
+    def __init__(self,shape_id,shape_type,*number):
         for num in number:
             self.check_numbers(num)
+        self.id = shape_id
+        self.shape_type = shape_type
         
     def check_numbers(self,number):
         if type(number) is bool or not isinstance(number,(int,float)):
@@ -16,5 +18,10 @@ class Shape:
     def get_perimeter(self):
         pass
     
+    def to_dict(self):
+        data = self.__dict__.copy()
+        if "shape_type" in data:
+            data["type"] = data.pop("shape_type")
+        return data
     def __str__(self):
         return self.__class__.__name__
