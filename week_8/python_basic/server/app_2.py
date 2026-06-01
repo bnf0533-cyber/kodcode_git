70ccad9b1d1e2007502cb37894dfb1ba4703d641
bnf0533-cyber <bnf0533@gmail.com>
2026-06-01 18:55:04 +0300

    add app_2 file

---
+++ week_8/python_basic/server/app_2.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+import uvicorn
+# app = FastAPI()
+
+# @app.get("/")
+# def read_root():
+#     return {"message" : "hello, I love coding"}
+# @app.get("/id")
+# def read_root():
+#     return {"message" : "hello, Ih id"}
+# @app.get("/items/{item_id}")
+# def get_item(item_id:int):
+#     return {"item_id" : item_id , "name" : f"item number {item_id}"}
+
+# if __name__ == "__main__":
+    # import uvicorn
+#     uvicorn.run("app_2:app", host="127.0.0.1",port=7900,reload=True)
+
+
+
