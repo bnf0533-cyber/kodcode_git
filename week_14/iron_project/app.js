4f234c73bbcdff12b4d08c7fdd0ad85d47e233dd
bnf0533-cyber <bnf0533@gmail.com>
2026-07-19 00:05:55 +0300

    feat: integrate central router into express server

--- week_14/iron_project/app.js
+++ week_14/iron_project/app.js
@@ -1,11 +1,13 @@
 import express from "express"
 import { createTables } from "./db/db_init.js"
+import router from "./routes/router.js"
 
 
 const PORT = process.env.PORT || 3000
 const app = express()
 
 app.use(express.json())
+app.use("/",router)
 await createTables()
 
 
