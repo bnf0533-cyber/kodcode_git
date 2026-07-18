94c8050e26407f7212a57fea46d3efaa49698bb7
bnf0533-cyber <bnf0533@gmail.com>
2026-07-18 22:06:10 +0300

    added to app file createTables

--- week_14/iron_project/app.js
+++ week_14/iron_project/app.js
@@ -1,11 +1,12 @@
 import express from "express"
+import { createTables } from "./db/db_init.js"
 
 
 const PORT = process.env.PORT || 3000
 const app = express()
 
 app.use(express.json())
-
+await createTables()
 
 
 app.listen(PORT , () => {
