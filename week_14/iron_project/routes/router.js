7180d2050451091a7300979133a2ab1c635a94b5
bnf0533-cyber <bnf0533@gmail.com>
2026-07-19 00:30:29 +0300

    feat: bind GET /incidents/open route to controller

--- week_14/iron_project/routes/router.js
+++ week_14/iron_project/routes/router.js
@@ -1,13 +1,15 @@
 import express from "express"
 import { insertNewOperator } from "../controllers/operatorsControllers.js"
+import { getOpenIncidents } from "../controllers/incidentsControllers.js"
+
 const router = express.Router()
 
 router.post("/operators", insertNewOperator)
 
-router.post("/incidents")
+// router.post("/incidents")
 
-router.patch("/incidents/:id/status")
+// router.patch("/incidents/:id/status")
 
-router.get("/incidents/open")
+router.get("/incidents/open", getOpenIncidents)
 
 export default router;
