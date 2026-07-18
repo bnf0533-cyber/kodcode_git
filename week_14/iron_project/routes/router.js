652ff6cb59c36e2a15ada6abb7691d0381966299
bnf0533-cyber <bnf0533@gmail.com>
2026-07-19 00:05:46 +0300

    feat: link operator routes to the controller

--- week_14/iron_project/routes/router.js
+++ week_14/iron_project/routes/router.js
@@ -1,4 +1,13 @@
 import express from "express"
-
+import { insertNewOperator } from "../controllers/operatorsControllers.js"
 const router = express.Router()
 
+router.post("/operators", insertNewOperator)
+
+router.post("/incidents")
+
+router.patch("/incidents/:id/status")
+
+router.get("/incidents/open")
+
+export default router;
