3a703bfc8437c2aeef35055bad392c6efb20cc5a
bnf0533-cyber <bnf0533@gmail.com>
2026-07-19 13:25:56 +0300

    feat: implement createNewIncident controller

--- week_14/iron_project/controllers/incidentsControllers.js
+++ week_14/iron_project/controllers/incidentsControllers.js
@@ -1,10 +1,21 @@
-import { fetchOpenIncidents } from "../services/incidentsService.js";
+import { createIncidentAndAddToLogs, fetchOpenIncidents } from "../services/incidentsService.js";
 
-export async function getOpenIncidents(req,res) {
+export async function getOpenIncidents(req, res) {
     try {
         const result = await fetchOpenIncidents()
         return res.status(200).json(result)
-    }catch (err) {
+    } catch (err) {
         console.error(err);
     }
+}
+
+export async function createNewIncident(req, res) {
+    try {
+        const {codeName , threatLevel , operatorId } = req.body
+        const result = await createIncidentAndAddToLogs(codeName , threatLevel , operatorId)
+        res.status(201).json(result)
+    } catch (err) {
+        console.error(err);
+    }
+
 }
