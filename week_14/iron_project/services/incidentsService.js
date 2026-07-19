ae1721db81e79e4658fb5800cb5a26ce05c34961
bnf0533-cyber <bnf0533@gmail.com>
2026-07-19 13:25:50 +0300

    feat: implement createIncidentAndAddToLogs in service

--- week_14/iron_project/services/incidentsService.js
+++ week_14/iron_project/services/incidentsService.js
@@ -1,5 +1,12 @@
-import { getOpenIncidents } from "../repositories/incidentsRepositories.js";
+import { createIncident, getOpenIncidents } from "../repositories/incidentsRepositories.js";
+import { addLogs } from "../repositories/logsRepositories.js";
 
 export async function fetchOpenIncidents() {
     return await getOpenIncidents()
+}
+
+export async function createIncidentAndAddToLogs(codeName , threatLevel , operatorId ) {
+    const query = await createIncident(codeName , threatLevel , operatorId)
+    const log = await addLogs("INCIDENT_CREATED" , query.id , operatorId , "New incident created")
+    return query
 }
