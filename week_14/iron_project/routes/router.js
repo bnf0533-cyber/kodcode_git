faea467db2f430312f91e500d6fb56c92a5e7788
bnf0533-cyber <bnf0533@gmail.com>
2026-07-19 14:30:50 +0300

    feat: bind PATCH /incidents/:id/status route

--- week_14/iron_project/routes/router.js
+++ week_14/iron_project/routes/router.js
@@ -1,6 +1,6 @@
 import express from "express"
 import { insertNewOperator } from "../controllers/operatorsControllers.js"
-import { getOpenIncidents,createNewIncident } from "../controllers/incidentsControllers.js"
+import { getOpenIncidents,createNewIncident, updateStatusIncident } from "../controllers/incidentsControllers.js"
 
 const router = express.Router()
 
@@ -8,7 +8,7 @@ router.post("/operators", insertNewOperator)
 
 router.post("/incidents",createNewIncident)
 
-// router.patch("/incidents/:id/status")
+router.patch("/incidents/:id/status", updateStatusIncident)
 
 router.get("/incidents/open", getOpenIncidents)
 
