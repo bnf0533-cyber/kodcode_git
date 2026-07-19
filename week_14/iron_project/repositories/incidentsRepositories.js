735511ced0893f70add637abe5c453e9b8ef23cd
bnf0533-cyber <bnf0533@gmail.com>
2026-07-19 13:25:43 +0300

    feat: add createIncident query to repository

--- week_14/iron_project/repositories/incidentsRepositories.js
+++ week_14/iron_project/repositories/incidentsRepositories.js
@@ -8,3 +8,19 @@ export async function getOpenIncidents() {
         console.error(err);
     }
 }
+
+export async function createIncident(codeName, threatLevel, operatorId) {
+    try {
+        const [query] = await pool.execute("insert into incidents (code_name , threat_level ,operator_id) values (?,?,?)", [codeName, threatLevel, operatorId])
+        return {
+            id :query.insertId,
+            codeName,
+            threatLevel,
+            operatorId,
+            status :"OPEN"
+        }
+    } catch (err) {
+        console.error(err);
+    }
+
+}
