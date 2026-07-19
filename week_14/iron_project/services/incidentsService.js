5b54f3421ab20c19bf4bb7d3b80bf457480ac05c
bnf0533-cyber <bnf0533@gmail.com>
2026-07-19 14:29:53 +0300

    feat: add status validation to updateStatus service

--- week_14/iron_project/services/incidentsService.js
+++ week_14/iron_project/services/incidentsService.js
@@ -1,5 +1,6 @@
-import { createIncident, getOpenIncidents } from "../repositories/incidentsRepositories.js";
+import { createIncident, getOpenIncidents,updateIncidentStatus } from "../repositories/incidentsRepositories.js";
 import { addLogs } from "../repositories/logsRepositories.js";
+import { getOperatorById } from "../repositories/operatorsRepositories.js";
 
 export async function fetchOpenIncidents() {
     return await getOpenIncidents()
@@ -8,5 +9,15 @@ export async function fetchOpenIncidents() {
 export async function createIncidentAndAddToLogs(codeName , threatLevel , operatorId ) {
     const query = await createIncident(codeName , threatLevel , operatorId)
     const log = await addLogs("INCIDENT_CREATED" , query.id , operatorId , "New incident created")
+    return query
+}
+
+export async function updateStatus(status , id , operatorId) {
+    const correctStatus = ["OPEN", "TRACKING", "INTERCEPTED", "CLOSED"]
+    if (!correctStatus.includes(status)) {
+        return "wrong status"
+    }
+    const query = await updateIncidentStatus(status , id)
+    const log = await addLogs("STATUS_UPDATED",id ,operatorId,"status change " + status)
     return query
 }
