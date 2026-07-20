85ccda5d1427121dd976fd2e37bd9b06d2c2cd2f
bnf0533-cyber <bnf0533@gmail.com>
2026-07-20 09:55:30 +0300

    cange tabs

--- week_14/iron_project/routes/router.js
+++ week_14/iron_project/routes/router.js
@@ -1,12 +1,12 @@
 import express from "express"
 import { insertNewOperator } from "../controllers/operatorsControllers.js"
-import { getOpenIncidents,createNewIncident, updateStatusIncident } from "../controllers/incidentsControllers.js"
+import { getOpenIncidents, createNewIncident, updateStatusIncident } from "../controllers/incidentsControllers.js"
 
 const router = express.Router()
 
 router.post("/operators", insertNewOperator)
 
-router.post("/incidents",createNewIncident)
+router.post("/incidents", createNewIncident)
 
 router.patch("/incidents/:id/status", updateStatusIncident)
 
