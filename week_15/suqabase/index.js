2696e98400789a9948dfa9a35133d34d9c24edb4
bnf0533-cyber <bnf0533@gmail.com>
2026-07-20 09:56:30 +0300

    change tabs

--- week_15/suqabase/index.js
+++ week_15/suqabase/index.js
@@ -1,35 +1,52 @@
 import { client } from "./db/db.js";
 
-
 export async function getAllUsers() {
-    const {data , error} = await client.from("users").select();
+    const { data, error } = await client.from("users").select();
     if (error) {
         return console.log(error);
     }
-    return data
+    return data;
 }
 
 export async function createUser(userName) {
-    const {data , error} = await client.from("users").insert({userName}).select().single()
+    const { data, error } = await client
+        .from("users")
+        .insert({ userName })
+        .select()
+        .single();
     if (error) return console.log(error);
-    return data
+    return data;
 }
 
 export async function deleteById(id) {
-    const {data , error} = await client.from("users").delete().eq("id",id).select().single()
+    const { data, error } = await client
+        .from("users")
+        .delete()
+        .eq("id", id)
+        .select()
+        .single();
     if (error) return console.log(error);
-    return data
+    return data;
 }
 
 export async function getById(id) {
-    const {data , error} = await client.from("users").select().eq("id",id).select().single()
+    const { data, error } = await client
+        .from("users")
+        .select()
+        .eq("id", id)
+        .select()
+        .single();
     if (error) return console.log(error);
-    return data
+    return data;
 }
 
-export async function updateUser(id , newData) {
-    const {data , error} = await client.from("users").update(newData).eq("id",id).select().single()
+export async function updateUser(id, newData) {
+    const { data, error } = await client
+        .from("users")
+        .update(newData)
+        .eq("id", id)
+        .select()
+        .single();
     if (error) return console.log(error);
-    return data
+    return data;
 }
-    
