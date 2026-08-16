65b7515d4f548000a48ac73e00855f267b41f7f1
bnf0533-cyber <bnf0533@gmail.com>
2026-08-16 09:20:05 +0300

    add all fronted

---
+++ week_18/dom/app.js
@@ -0,0 +1,23 @@
+// console.log('hello from vs');
+// let title = document.querySelector('h1')
+// let titleText = document.querySelector('h1').textContent
+// console.log(titleText);
+
+// let h1 = document.body.querySelector('h1').textContent = "hello mustafa"
+// console.log(h1);
+
+// const item = document.querySelectorAll("#list");
+// console.log(item);
+
+// item.forEach((el) => {
+//     console.log(el.textContent);
+
+// })
+
+// let res = document.body.querySelector('#greething')
+// console.log(res.textContent = 'BYE');
+
+const player = document.querySelectorAll(".players");
+player.forEach((p) => {
+    console.log(p.textContent)
+});
