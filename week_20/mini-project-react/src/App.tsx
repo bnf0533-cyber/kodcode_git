ca315db3138649d4e0d9ad6364465c2ae08ed2c8
bnf0533-cyber <bnf0533@gmail.com>
2026-08-31 11:51:25 +0300

    add mini project

---
+++ week_20/mini-project-react/src/App.tsx
@@ -0,0 +1,16 @@
+export interface  Book {
+  id : string;
+  title: string;
+  author : string;
+  pages : number;
+  finished : boolean;
+}
+
+export interface Movie {
+  id: string;
+  title: string;
+  director: string;
+  durationMinutes: number;
+  finished : boolean;
+}
+
